24a462acd47ce974ab15cc2d5b0153e3bb16eab8
mtweeman <mtweeman@gmail.com>
2022-03-10 21:26:28 +0100

    Removed application window preset

--- app/src/application_window.py
+++ app/src/application_window.py
@@ -3,21 +3,12 @@ from tkinter import *
 
 class ApplicationWindow(Tk):
     def __init__(self):
-        self.__preset_window()
-
-        super().__init__()
-
         self.__name = "Hajle Silesia Homebrewing System"
         self.__icon_path = "./img/icon.png"
 
+        super().__init__()
+
         self.__set_window()
-
-    def __preset_window(self):
-        self.__set_dpi_awareness()
-
-    @staticmethod
-    def __set_dpi_awareness():
-        pass
 
     def __set_window(self):
         self.__set_titlebar_icon()
