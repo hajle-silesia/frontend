cdd76d74b283f2cc5767ba106aaba98d441a6e77
mtweeman <mtweeman@gmail.com>
2023-10-12 20:21:56 +0200

    Added last recipe shown as default

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM nginx:1.23.3
+FROM nginx:1.25.2
 RUN apt update && apt install -y \
     curl \
     python3 \
--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 import js
+import asyncio
 
 from brewing import create_brewing, show
 from menu import create_menu, activate_tab
 from recipe import create_recipe
 
 
-def main():
+async def main():
     js.document.title = "Hajle Silesia Homebrewing System"
 
     create_menu()
-    create_recipe()
+    await create_recipe()
     create_brewing()
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.gather(main())
