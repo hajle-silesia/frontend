38a226d4ecc251eb6691291fda338516b6e5ad7d
mtweeman <mtweeman@gmail.com>
2024-12-20 20:35:26 +0000

    fix: try simple script to investigate website not rendering

--- src/main.py
+++ src/main.py
@@ -1,18 +1,24 @@
-import js
-import asyncio
+# import js
+# import asyncio
+#
+# from brewing import create_brewing, show
+# from menu import create_menu, activate_tab
+# from recipe import create_recipe
+#
+#
+# async def main():
+#     js.document.title = "Hajle Silesia Homebrewing System"
+#
+#     create_menu()
+#     await create_recipe()
+#     create_brewing()
+#
+#
+# if __name__ == '__main__':
+#     asyncio.gather(main())
+from pyscript import display
+from datetime import datetime
 
-from brewing import create_brewing, show
-from menu import create_menu, activate_tab
-from recipe import create_recipe
-
-
-async def main():
-    js.document.title = "Hajle Silesia Homebrewing System"
-
-    create_menu()
-    await create_recipe()
-    create_brewing()
-
-
-if __name__ == '__main__':
-    asyncio.gather(main())
+now = datetime.now()
+display("Hello from the other side!")
+display(now.strftime("%m/%d/%Y, %H:%M:%S"))
