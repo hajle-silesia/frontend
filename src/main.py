da1778f4931bf87dda0c525db0133fcf28c6d5cb
mtweeman <mtweeman@gmail.com>
2024-12-31 08:54:02 +0000

    revert: fix: try simple script to investigate website not rendering (#19)
    This reverts commit 365744a6538ebe8028456f6f2516c3f1520fbb36.

--- src/main.py
+++ src/main.py
@@ -1,24 +1,18 @@
-# import js
-# import asyncio
-#
-# from brewing import create_brewing, show
-# from menu import create_menu, activate_tab
-# from recipe import create_recipe
-#
-#
-# async def main():
-#     js.document.title = "Hajle Silesia Homebrewing System"
-#
-#     create_menu()
-#     await create_recipe()
-#     create_brewing()
-#
-#
-# if __name__ == '__main__':
-#     asyncio.gather(main())
-from pyscript import display
-from datetime import datetime
+import js
+import asyncio
 
-now = datetime.now()
-display("Hello from the other side!")
-display(now.strftime("%m/%d/%Y, %H:%M:%S"))
+from brewing import create_brewing, show
+from menu import create_menu, activate_tab
+from recipe import create_recipe
+
+
+async def main():
+    js.document.title = "Hajle Silesia Homebrewing System"
+
+    create_menu()
+    await create_recipe()
+    create_brewing()
+
+
+if __name__ == '__main__':
+    asyncio.gather(main())
