27b44f736ba6008d7af2ce956bb66d2793370502
mtweeman <mtweeman@gmail.com>
2025-01-23 12:06:11 +0000

    fix: use secure video streaming protocol

--- src/brewing.py
+++ src/brewing.py
@@ -40,7 +40,7 @@ def create_brewing():
             var videoElement = document.getElementById('mlt_sightglass_video');
             var flvPlayer = flvjs.createPlayer({
                 type: 'flv',
-                url: 'http://srs-server.default.svc.cluster.local:8080/live/livestream.flv'
+                url: 'https://srs-server.default.svc.cluster.local:8080/live/livestream.flv'
             });
             flvPlayer.attachMediaElement(videoElement);
             flvPlayer.load();
