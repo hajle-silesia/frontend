8435fb8d9a7b54c53c68f56cc80f8a10e448c190
mtweeman <mtweeman@gmail.com>
2025-01-27 08:44:56 +0000

    docs: document video stream

--- src/scripts/video-streaming.js
+++ src/scripts/video-streaming.js
@@ -1,3 +1,7 @@
+// This file is parsed by Python's str.format() function.
+// For this reason, doubling curly braces is required to escape those that are not part of the formatting placeholders.
+// The final output will contain single curly braces as intended in JavaScript code.
+
 if (flvjs.isSupported()) {{
     var videoElement = document.getElementById('{element_id}');
     var flvPlayer = flvjs.createPlayer({{
